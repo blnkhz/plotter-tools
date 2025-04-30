705551c70119f957e9d8088a6aa084a0a4225ba5
Blanka Hooz <hooz.blanka@gmail.com>
2025-04-30 13:59:35 -0700

    create three.js viewer component

---
+++ plotter-tools/src/components/ThreeJsViewer.jsx
@@ -0,0 +1,41 @@
+import React, { useRef, useEffect } from 'react'
+import useThreeJsScene from '../hooks/useThreeJsScene'
+import { createWireframeMesh } from '../utils/geometryUtils'
+
+const ThreeJsViewer = ({
+  geometry,
+  width = 500,
+  height = 500,
+  onSceneReady,
+}) => {
+  const mountRef = useRef()
+  const { clearScene, addToScene, scene, camera, renderer, controls } =
+    useThreeJsScene(mountRef, width, height)
+
+  useEffect(() => {
+    if (camera && onSceneReady) {
+      onSceneReady({ scene, camera, renderer, controls })
+    }
+  }, [scene, camera, renderer, controls, onSceneReady])
+
+  useEffect(() => {
+    if (!geometry) return
+
+    clearScene()
+    const mesh = createWireframeMesh(geometry)
+    addToScene(mesh)
+  }, [geometry, clearScene, addToScene])
+
+  return (
+    <div
+      ref={mountRef}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        border: '1px solid #ccc',
+      }}
+    />
+  )
+}
+
+export default ThreeJsViewer
