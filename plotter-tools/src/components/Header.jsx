d139186c83baf80e9c5f210d8f2312cf566d3ea8
Blanka Hooz <hooz.blanka@gmail.com>
2025-05-02 16:18:53 -0700

    create header component

---
+++ plotter-tools/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import AboutModal from './AboutModal'
+
+const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
+
+  return (
+    <div className="w-full min-w-full text-white shadow-md">
+      <div className="w-full px-2 sm:px-6 py-2 flex justify-between items-center">
+        <h1 className="text-xl text-green-700">makeshift plotter tools</h1>
+        <button
+          onClick={openModal}
+          className="px-4 py-2 bg-rose-100 text-green-700 rounded hover:bg-rose-200 transition-colors duration-200"
+        >
+          about
+        </button>
+      </div>
+
+      {isModalOpen && <AboutModal closeModal={closeModal} />}
+    </div>
+  )
+}
+
+export default Header
