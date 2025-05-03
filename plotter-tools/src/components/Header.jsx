00bb6e01dafa50a5c3ce1eb75483fe4c4d9b7e82
Blanka Hooz <hooz.blanka@gmail.com>
2025-05-02 17:01:52 -0700

    rename site

--- plotter-tools/src/components/Header.jsx
+++ plotter-tools/src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <div className="w-full min-w-full text-white shadow-md">
       <div className="w-full px-2 sm:px-6 py-2 flex justify-between items-center">
-        <h1 className="text-xl text-green-700">makeshift plotter tools</h1>
+        <h1 className="text-xl text-green-700">blanka's plotter tools</h1>
         <button
           onClick={openModal}
           className="px-4 py-2 bg-rose-100 text-green-700 rounded hover:bg-rose-200 transition-colors duration-200"
